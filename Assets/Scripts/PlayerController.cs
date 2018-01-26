a9fe9d41d573b915060376ab549a05fdb74398d7
ftemplin <Fritz.templin@gmail.com>
2018-01-25 19:16:22 -0800

    Added Player movement and collison for Video

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rigidbody;
+    public float walkSpeed = 1;
+    public float turnSpeed = 1;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate () {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = transform.forward * moveVertical * walkSpeed;
+        rigidbody.angularVelocity = transform.up * moveHorizontal * turnSpeed;
+
+    }
+}
---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+    public VideoPlayer videoPlayer;
+    void OnTriggerExit (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            videoPlayer.Stop();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            videoPlayer.Play();
+        }
+    }
+}
